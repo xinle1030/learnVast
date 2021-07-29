05b8c2dce4192e0ec650ed27c2ebdc00cbf6a1db
LAMXINLE <xlam0003@student.monash.edu>
2021-07-30 00:14:13 +0800

    adjusting landing header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,16 @@
-var nav = document.querySelector('nav');
+let nav = document.querySelector('nav');
 
 // when there is a scroll event to this document
 window.addEventListener('scroll', function() {
     // find the nav bar using the nav bar id
-    var $nav = $("#mainNavbar");
+    let $nav = $("#mainNavbar");
     // once we scroll the page top more than the navbar height, we will detect the scroll and change the nav bar scc properties using toggleClass method on .scrolled class
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
 });
+
+let letters = document.querySelectorAll(".letter");
+for (let i = 0; i < letters.length; i++) {
+    letters[i].addEventListener("mouseover", () => {
+        letters[i].classList.toggle("flip-animation");
+    });
+}
